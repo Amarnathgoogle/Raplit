cdd5e1c1332ca64b85f2233489bc961558fbdd80
Amarnathgoogle <151923577+Amarnathgoogle@users.noreply.github.com>
2024-05-04 14:41:51 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,53 @@
-from flask import Flask
-from threading import Thread
+import discord
+import asyncio
+import websockets
 
-app = Flask('')
+client = discord.Client()
 
-@app.route('/')
-def main():
-    return '<meta http-equiv="refresh" content="0; URL=https://phantom.is-a.dev/support"/>'
+async def send_message(channel, message):
+    await channel.send(message)
 
-def run():
-    app.run(host="0.0.0.0", port=8080)
+async def read_messages(channel):
+    async with websockets.connect(f'wss://gateway.discord.gg/?v=6&encording=json&compress=zlib-stream') as websocket:
+        await websocket.send(json.dumps({
+            'op': 2,
+            'd': {
+                'token': client.http.token,
+                'properties': {
+                    '$os': 'linux',
+                    '$browser': 'discord.py',
+                    '$device': 'discord.py'
+                }
+            }
+        }))
+        while True:
+            message = await websocket.recv()
+            message = json.loads(message)
+            if message['t'] == 'MESSAGE_CREATE':
+                if message['d']['channel_id'] == channel.id:
+                    await send_message(channel, f'{message["d"]["author"]["username"]}: {message["d"]["content"]}')
 
-def keep_alive():
-    server = Thread(target=run)
-    server.start()
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    await client.change_presence(activity=discord.Game(name="I am alive"))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$ping'):
+        await message.channel.send('Pong!')
+
+    if message.content.startswith('$status'):
+        status = message.content.replace('$status ', '')
+        await client.change_presence(activity=discord.Game(name=status))
+        await message.channel.send(f'Status changed to {status}')
+
+    if message.content.startswith('$read'):
+        channel = message.channel
+        await message.channel.send('Reading messages...')
+        await read_messages(channel)
+
+client.run('MTA0MDI1NzQzODE1NjMyODk2MA.GC_Y1y.ExN5KZIo465IqfabWIc5pcQHKHT1jp3eJGswXs')
